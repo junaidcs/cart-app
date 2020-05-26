7b5deab862edcbb95f0711767499200863c93f62
Muhammad Junaid <m.junaidcs@gmail.com>
2020-05-26 23:30:15 +0500

    dummy intro screen

---
+++ screens/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const IntroScreens = () => {
+    return (
+        <View>
+            <Text>This is first Intro Screen</Text>
+        </View>
+    );
+}
+
+export default IntroScreens;
