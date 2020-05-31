ed4e495c81d3c7e228b9d77e4910023b52240b98
Muhammad Junaid <m.junaidcs@gmail.com>
2020-05-31 21:31:01 +0500

    dummy tabs

---
+++ components/BottomNavigatoin.js
@@ -0,0 +1,30 @@
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { View, Text } from 'react-native';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+const SettingsScreen = () => {
+    return (
+        <View>
+            <Text>Settings Screen</Text>
+        </View>
+    )
+}
+const MyTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default MyTabs;
