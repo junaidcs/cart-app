f1191b541d8e7aa86f63a6ed4b622a37b5a3c6f8
Muhammad Junaid <m.junaidcs@gmail.com>
2020-06-01 22:08:09 +0500

    Welcome screen dummy

---
+++ screens/Welcom.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { BottomNavigation } from 'react-native-paper'
+
+const Welcome = () => {
+    return (
+        <View>
+            <Text>Hello Junaid!</Text>
+            <BottomNavigation />
+        </View>
+    );
+}
+
+export default Welcome;
